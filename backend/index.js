90623e7f17560477f2fd92c064906256de5dd523
NozarLahooti <nozarlahooti@me.com>
2025-07-16 23:08:53 -0400

    Successful connection to db

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,19 @@
 import express from 'express'
 import cors from 'cors'
-const app = express()
+import 'dotenv/config'
+import connectDb from './db.js'
 
+
+const app = express()
 const port = 8080
 app.use(cors())
 
 
 app.get('/', (req, res) => {
     res.json('Hello (from server)')
+    
 })
-app.listen(port, () => console.log('listining on port:' + port))
+app.listen(port, () => {
+     console.log('listining on port:' + port)
+     connectDb()
+    })
