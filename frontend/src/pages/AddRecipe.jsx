73f4da045bdf664449c5459d33a05d587830a97a
NozarLahooti <nozarlahooti@me.com>
2025-07-18 00:43:47 -0400

    add form

--- frontend/src/pages/AddRecipe.jsx
+++ frontend/src/pages/AddRecipe.jsx
@@ -2,6 +2,13 @@ export default function AddRecipe() {
   return (
     <div>
       <h1>Add New Recipe</h1>
+      <form>
+        <input type="text" placeholder="Title" />
+        <input type="text" placeholder="Ingredients (comma separated)" />
+        <textarea placeholder="Instructions"></textarea>
+        <input type="text" placeholder="Image URL" />
+        <button type="submit">Add Recipe</button>
+      </form>
     </div>
   );
 }
