b30e09b6c509e3e53a1f526b112498a3a48274c0
NozarLahooti <nozarlahooti@me.com>
2025-07-19 22:04:56 -0400

    Add MongoDB indexes

--- backend/models/orderino.js
+++ backend/models/orderino.js
@@ -7,4 +7,10 @@ const orderinoSchema = mongoose.Schema({
   imageURL: { type: String },
 }, { timestamps: true });
 
+// Index on title for faster searches by title
+orderinoSchema.index({ title: 1 });
+
+// Index on creation time for sorting newest first
+orderinoSchema.index({ createAt: -1});
+
 export default mongoose.model('Recipe', orderinoSchema);
